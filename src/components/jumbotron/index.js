7b21ac7f02bed9f20ce3314175a7ae0459acfc6b
Rishik Kumar <rishik.k2002@gmail.com>
2022-02-25 15:08:56 +0530

    added text to frontent

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Container, Inner } from './styles/jumbotron';
+import { Container, Inner, Pane, Title, SubTitle, Image } from './styles/jumbotron';
 export default function Jumbotron({ children, direction = 'row', ...restProps}){
     return(
     <Inner direction={direction}>
-        <p>i am jumbo</p>
+        { children }
     </Inner>
     );
 }
 
 Jumbotron.Container = function JumboContainer({children, ...restProps}){
-    return <Container { ...restProps}>{children}</Container>
+    return <Container { ...restProps}>{children}</Container>;
+}
+
+Jumbotron.Pane = function JumboPane({children, ...restProps}){
+    return <Pane { ...restProps}>{children}</Pane>;
+}
+
+Jumbotron.Title = function JumboContainer({children, ...restProps}){
+    return <Title { ...restProps}>{children}</Title>;
+}
+
+Jumbotron.SubTitle = function JumboContainer({children, ...restProps}){
+    return <SubTitle { ...restProps}>{children}</SubTitle>;
+}
+
+Jumbotron.Image = function JumboImage({ ...restProps}){
+    return <Image { ...restProps} />;
 }
