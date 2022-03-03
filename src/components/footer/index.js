ca2a7fe310b090b370f6fec854c25a35f21e1019
Rishik Kumar <rishik.k2002@gmail.com>
2022-03-03 10:43:38 +0530

    making footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Container } from "../jumbotron/styles/jumbotron";
+export default function Footer({ children, ...restProps}){
+    return <Container { ...restProps }>{children}</Container>
+}
+
+Footer.Row = function
