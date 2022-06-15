1e0f61384c8422be1136a4be874b460cdbdd7a67
Rishik Kumar <rishik.k2002@gmail.com>
2022-06-16 01:31:29 +0530

    footerbreak

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -24,3 +24,7 @@ Footer.Title = function FooterTitle({ children, ...restProps }){
 Footer.Text = function FooterText({ children, ...restProps }){
     return <Text { ...restProps}>{children}</Text>;
 };
+
+Footer.Break = function FooterBreak({children, ...restProps}){
+    return <Break {...restProps}>{children}</Break>;
+}
