6333279687dfb03ffeae9c89becaa4cd6326165e
Rishik Kumar <rishik.k2002@gmail.com>
2022-06-16 02:02:04 +0530

    footer js

---
+++ src/containers/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Footer} from '../components';
+export function FooterContainer(){
+    return(
+        <Footer>
+            <Footer.Title>Questions? Contact us.</Footer.Title>
+        </Footer>
+    );
+}
